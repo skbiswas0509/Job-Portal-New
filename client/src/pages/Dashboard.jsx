4648520d32d3c686147d6646af68252021a8d87f
Sukumar Biswas <sukumarbsws98@gmail.com>
2025-04-09 20:57:55 +0600

    ViewApplications

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { NavLink, Outlet, useNavigate } from 'react-router-dom'
 import { assets } from '../assets/assets'
 
 const Dashboard = () => {
+
+    const navigate = useNavigate()
+
   return (
     <div className='min-h-screen'>
         {/* navbar for recruiter panel */}
         <div className='shadow py-4'>
             <div className='px-5 flex justify-between items-center'>
-                <img className='max-sm:w-32 cursor-pointer' src={assets.logo} alt="" />
+                <img onClick={e => navigate('/')} className='max-sm:w-32 cursor-pointer' src={assets.logo} alt="" />
                 <div className='flex items-center gap-3'>
                     <p className='max-sm:hidden'>Welcome, Skbiswas</p>
                     <div className='relative group'>
@@ -20,6 +23,33 @@ const Dashboard = () => {
                         </div>
                     </div>
                 </div>
+            </div>
+        </div>
+
+        <div className='flex items-start'>
+
+            {/* left side bar with option to add job, manage job and view applications */}
+            <div className='inline-block min-h-screen border-r-2'>
+                <ul className='flex flex-col items-start pt-5 text-gray-800'>
+                    <NavLink className={({isActive})=> `flex items-center p-3 sm:px-6 gap-2 w-full hover:bg-gray-100 ${isActive && 'bg-blue-200 border-r-4 border-blue-500'}`} to={'/dashboard/add-job'}>
+                        <img className='min-w-4' src={assets.add_icon} alt="" />
+                        <p className='max-sm:hidden'>Add Job</p>
+                    </NavLink>
+
+                    <NavLink className={({isActive})=> `flex items-center p-3 sm:px-6 gap-2 w-full hover:bg-gray-100 ${isActive && 'bg-blue-200 border-r-4 border-blue-500'}`} to={'/dashboard/manage-jobs'}>
+                        <img className='min-w-4' src={assets.home_icon} alt="" />
+                        <p className='max-sm:hidden'>Manage Job</p>
+                    </NavLink>
+
+                    <NavLink className={({isActive})=> `flex items-center p-3 sm:px-6 gap-2 w-full hover:bg-gray-100 ${isActive && 'bg-blue-200 border-r-4 border-blue-500'}`} to={'/dashboard/view-applications'}>
+                        <img className='min-w-4' src={assets.person_tick_icon} alt="" />
+                        <p className='max-sm:hidden'>View Applications</p>
+                    </NavLink>
+                </ul>
+            </div>
+
+            <div>
+                <Outlet />
             </div>
         </div>
     </div>
--- client/src/pages/ViewApplications.jsx
+++ client/src/pages/ViewApplications.jsx
@@ -1,8 +1,51 @@
 import React from 'react'
+import { assets, viewApplicationsPageData } from '../assets/assets'
 
 const ViewApplications = () => {
   return (
-    <div>ViewApplications</div>
+    <div className='container mx-auto p-4'>
+      <div>
+        <table className='w-full max-w-4xl bg-white border border-gray-200 max-sm:text-sm'>
+          <thead>
+            <tr className='border-b'>
+              <th className='py-2 px-4 text-left'>#</th>
+              <th className='py-2 px-4 text-left'>Username</th>
+              <th className='py-2 px-4 text-left max-sm:hidden'>Job Title</th>
+              <th className='py-2 px-4 text-left max-sm:hidden'>Location</th>
+              <th className='py-2 px-4 text-left'>Resume</th>
+              <th className='py-2 px-4 text-left'>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {viewApplicationsPageData.map((applicant,index)=>(
+              <tr key={index} className='text-gray-700'>
+                <td className='py-2 px-4 border-b text-center'>{index+1}</td>
+                <td className='py-2 px-4 border-b text-center flex'>
+                  <img className='w-10 h-10 rounded-full mr-3 max-sm:hidden' src={applicant.imgSrc} alt="" />
+                  <span>{applicant.name}</span>
+                </td>
+                <td>{applicant.jobTitle}</td>
+                <td>{applicant.location}</td>
+                <td>
+                  <a href="" target='_blank'>
+                    Resume <img src={assets.resume_download_icon} alt="" />
+                  </a>
+                </td>
+                <td>
+                  <div>
+                    <button>...</button>
+                    <div>
+                      <button>Accept</button>
+                      <button>Reject</button>
+                    </div>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
   )
 }
 
